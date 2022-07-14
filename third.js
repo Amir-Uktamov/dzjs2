5c88e6527f52a481dd094aed5d638670095d85b4
amir <blablabla@gmail.com>
2022-07-14 12:06:04 +0500

    dzjs2

---
+++ third.js
@@ -0,0 +1,21 @@
+a = +prompt("Введите число:"); 
+b = +prompt("Введите число:"); 
+c = +prompt("Введите число:");
+	
+if (a>b && b<c) {
+    var d = b;
+	alert(d);
+}
+	
+
+if (a<b && a>c) {
+    var d = a;
+	alert(d);
+}
+		
+
+if (c<a && c>b) {
+    var d = c;
+	alert(d);
+}
+			
